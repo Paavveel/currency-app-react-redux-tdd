1cff4f0cd7d48dc1e97589e43af53eb5bc95767a
Paavveel <pa.tomilin@gmail.com>
2022-08-17 17:40:37 +0300

    Add Quote styles, render the component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './App.module.css';
+import { Quote } from './features/Quote';
 
 export function App() {
   return (
     <main className={styles.main}>
       {/* Header */}
       {/* Converter form */}
-      {/* Footer */}
+      <Quote />
     </main>
   );
 }
--- src/features/Quote/Quote.js
+++ src/features/Quote/Quote.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import styles from './Quote.module.css';
 import { quotes } from './quotes';
 
 export function Quote() {
   const { text, author } = quotes[0];
   return (
-    <>
+    <footer className={styles.quote}>
       <blockquote>{text}</blockquote>
-      <cite>- {author}</cite>
-    </>
+      <cite className={styles.author}>- {author}</cite>
+    </footer>
   );
 }
