9b204037235681608c8fbc6f5b7f3db0aa744c76
Paavveel <pa.tomilin@gmail.com>
2022-08-17 17:21:57 +0300

    Render a quote author

--- src/features/Quote/Quote.js
+++ src/features/Quote/Quote.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { quotes } from './quotes';
 
 export function Quote() {
-  const quote = quotes[0];
-  return <blockquote>{quote.text}</blockquote>;
+  const { text, author } = quotes[0];
+  return (
+    <>
+      <blockquote>{text}</blockquote>
+      <cite>- {author}</cite>
+    </>
+  );
 }
--- src/features/Quote/Quote.test.js
+++ src/features/Quote/Quote.test.js
@@ -3,10 +3,16 @@ import { Quote } from './Quote';
 import { quotes } from './quotes';
 
 describe('when rendered', () => {
-  const testQuote = quotes[0];
+  const { text, author } = quotes[0];
   it('should contain an expected text', () => {
     render(<Quote />);
-    const result = screen.getByText(testQuote.text);
+    const result = screen.getByText(new RegExp(text));
+    expect(result).toBeInTheDocument();
+  });
+
+  it('should contain an expected author', () => {
+    render(<Quote />);
+    const result = screen.getByText(new RegExp(author));
     expect(result).toBeInTheDocument();
   });
 });
