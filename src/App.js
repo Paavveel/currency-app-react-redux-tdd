eabc170d030f13c4bd4f7a4491d1f05cb2a9b9b7
Paavveel <pa.tomilin@gmail.com>
2022-08-17 14:45:35 +0300

    Cleanup App component, use CSS-modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
 
 export function App() {
-  return <div className='App'></div>;
+  return <main className={styles.main}></main>;
 }
