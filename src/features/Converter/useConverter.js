c59fcdba1c089d9679721a51a07003a903d35d3f
Paavveel <pa.tomilin@gmail.com>
2022-08-18 17:21:51 +0300

    Implement two-way value binding in the converter hook

--- src/features/Converter/useConverter.js
+++ src/features/Converter/useConverter.js
@@ -1,13 +1,30 @@
 import { useState } from 'react';
+import { rubToUsd, usdToRub } from '../../domain/converter';
 
-export const useConverter = () => {
-  const [rub, setRub] = useState(100);
-  const [usd, setUsd] = useState(1);
+export const useConverter = (initialRubValue, course) => {
+  const calculatedUsdAmount = rubToUsd(initialRubValue, course);
+  const [rub, setRub] = useState(initialRubValue);
+  const [usd, setUsd] = useState(calculatedUsdAmount);
+
+  const updateRub = value => {
+    const rub = value;
+    const usd = rubToUsd(rub, course);
+
+    setRub(rub);
+    setUsd(usd);
+  };
+  const updateUsd = value => {
+    const usd = value;
+    const rub = usdToRub(usd, course);
+
+    setRub(rub);
+    setUsd(usd);
+  };
 
   return {
     rub,
     usd,
-    updateRub: setRub,
-    updateUsd: setUsd,
+    updateRub,
+    updateUsd,
   };
 };
--- src/features/Converter/useConverter.test.js
+++ src/features/Converter/useConverter.test.js
@@ -1,16 +1,64 @@
+import { act, renderHook } from '@testing-library/react';
+import { useConverter } from './useConverter';
+
+const course = 42;
+const testRubAmount = 100;
+const calculatedUsdAmount = 2.38;
+
 describe('when rendered', () => {
-  it.todo('RUB input should have a value equal to initial RUB amount');
-  it.todo('USD input should have a value equal to calculated USD amount');
+  it('RUB value should be equal to initial RUB amount', () => {
+    const { result } = renderHook(() => useConverter(testRubAmount, course));
+    expect(result.current.rub).toEqual(testRubAmount);
+  });
+
+  it('USD value should be equal to calculated USD amount', () => {
+    const { result } = renderHook(() => useConverter(testRubAmount, course));
+    expect(result.current.usd).toEqual(calculatedUsdAmount);
+  });
 });
 
 describe('when called an `updateRub` method', () => {
-  it.todo('should update the RUB value');
-  it.todo('should recalculate the USD value');
+  it('should update the RUB value', () => {
+    const { result } = renderHook(() => useConverter(testRubAmount, course));
+
+    act(() => {
+      result.current.updateRub(10);
+    });
+
+    expect(result.current.rub).toEqual(10);
+  });
+
+  it('should recalculate the USD value', () => {
+    const { result } = renderHook(() => useConverter(testRubAmount, course));
+
+    act(() => {
+      result.current.updateRub(10);
+    });
+
+    expect(result.current.usd).toEqual(0.24);
+  });
 });
 
 describe('when called an `updateUsd` method', () => {
-  it.todo('should update the USD value');
-  it.todo('should recalculate the RUB value');
+  it('should update the USD value', () => {
+    const { result } = renderHook(() => useConverter(testRubAmount, course));
+
+    act(() => {
+      result.current.updateUsd(10);
+    });
+
+    expect(result.current.usd).toEqual(10);
+  });
+
+  it('should recalculate the RUB value', () => {
+    const { result } = renderHook(() => useConverter(testRubAmount, course));
+
+    act(() => {
+      result.current.updateUsd(10);
+    });
+
+    expect(result.current.rub).toEqual(420);
+  });
 });
 
 describe('when re-rendered', () => {
