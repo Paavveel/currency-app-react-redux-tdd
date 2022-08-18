345863f9134d08e255dae5b0a85e1fadeaa8e57a
Paavveel <pa.tomilin@gmail.com>
2022-08-18 14:01:53 +0300

    Use controlled inputs

---
+++ src/features/Converter/Converter.test.js
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Converter } from './Converter';
+
+describe('when rendered', () => {
+  it('rub input should have a value with a rub amount', () => {
+    render(<Converter />);
+    expect(screen.getByLabelText(/Сумма в рублях/)).toHaveValue(100);
+  });
+
+  it('usd input should have a value with a usd amount', () => {
+    render(<Converter />);
+    expect(screen.getByLabelText(/Сумма в долларах/)).toHaveValue(1);
+  });
+});
+
+describe('when typed in a RUB input', () => {
+  it('should update its value', async () => {
+    const user = userEvent.setup();
+    render(<Converter />);
+    const input = screen.getByLabelText(/Сумма в рублях/);
+
+    user.clear(input);
+    await user.type(input, '42');
+    expect(input).toHaveValue(42);
+  });
+});
+
+describe('when typed in a USD input', () => {
+  it('should update its value', async () => {
+    const user = userEvent.setup();
+    render(<Converter />);
+    const input = screen.getByLabelText(/Сумма в долларах/);
+
+    user.clear(input);
+    await user.type(input, '42');
+    expect(input).toHaveValue(42);
+  });
+});
