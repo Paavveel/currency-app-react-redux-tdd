0eb03a48742064963a510ac3093597cb1da0b7db
Pavel T <pa.tomilin@gmail.com>
2022-08-20 14:13:30 +0300

    Add a Course and a Header components draft

---
+++ src/features/Course/CurrentCourse.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './CurrentCourse.module.css';
+
+export function CurrentCourse() {
+  return <div className={styles.value}>Курса доллара сегодня:</div>;
+}
