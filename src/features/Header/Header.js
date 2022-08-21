62151df5ad5b5998b319c14736ee767457c39999
Pavel T <pa.tomilin@gmail.com>
2022-08-21 13:42:31 +0300

    Add a RefreshCourse component draft

--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { CurrentCourse } from '../Course/CurrentCourse';
+import { CurrentCourse } from '../Course';
+import { RefreshCourse } from '../RefreshCourse';
 import styles from './Header.module.css';
 
 export function Header() {
   return (
     <header className={styles.header}>
-      Курса доллара сегодня: <CurrentCourse />
+      Курса доллара сегодня:
+      <CurrentCourse />
+      <RefreshCourse />
     </header>
   );
 }
---
+++ src/features/RefreshCourse/RefreshCourse.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const RefreshCourse = () => {
+  return <button type='button'>Обновить курс</button>;
+};
