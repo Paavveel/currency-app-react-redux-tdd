b90a477835266b3fe304de460c313054f0cd47dd
Paavveel <pa.tomilin@gmail.com>
2022-08-22 14:20:50 +0300

    Cleanup features folder and testUtils index

--- src/testUtils/index.js
+++ src/testUtils/index.js
@@ -1,14 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-// import thunk from 'redux-thunk';
 import courseReducer, {
   initialCourseState,
 } from '../features/Course/courseSlice';
-
-// const reducer = combineReducers({
-//   course: courseReducer,
-// });
 
 const originalState = {
   course: initialCourseState,
@@ -23,7 +18,6 @@ export const renderWithStore = (
       course: courseReducer,
     },
     preloadedState: state,
-    // middleware: getDefaultMiddleware => getDefaultMiddleware().concat(thunk),
   });
   return render(<Provider store={storeMock}>{ui}</Provider>);
 };
